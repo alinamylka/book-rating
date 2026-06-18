8d176ee89be2bf7b40f895fc2209d04bb2a33a87
u216116 <alina_maria.mylka@sbb.ch>
2026-06-18 11:09:41 +0200

    feat: book rating helper

--- src/app/books/shared/book-rating-helper.ts
+++ src/app/books/shared/book-rating-helper.ts
@@ -4,9 +4,19 @@ import {Service} from '@angular/core';
 @Service()
 export class BookRatingHelper {
   rateUp(book: Book) {
-    return book;
+    if (book.rating >= 5) {
+      return book;
+    }
+    return {
+      ...book,
+      rating: book.rating + 1,
+    }
   }
+
   rateDown(book: Book) {
-    return book;
+    return {
+      ...book,
+      rating: Math.max(1, book.rating - 1),
+    }
   }
 }
