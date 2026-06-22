b1bda4916cc9e9263d99dddc6fce20e1249a89ad
u216116 <alina_maria.mylka@sbb.ch>
2026-06-22 15:03:11 +0200

    feat: errors

--- src/app/books/book-create-page/book-create-page.ts
+++ src/app/books/book-create-page/book-create-page.ts
@@ -1,6 +1,15 @@
 import {Component, signal} from '@angular/core';
 import {Book} from '../shared/book';
-import {form, FormField, max, maxLength, min, minLength, required} from '@angular/forms/signals';
+import {
+  form,
+  FormField,
+  max,
+  maxLength,
+  min,
+  minLength,
+  provideSignalFormsConfig,
+  required
+} from '@angular/forms/signals';
 import {JsonPipe} from '@angular/common';
 
 @Component({
@@ -11,6 +20,11 @@ import {JsonPipe} from '@angular/common';
   ],
   templateUrl: './book-create-page.html',
   styleUrl: './book-create-page.scss',
+  providers: [provideSignalFormsConfig({
+    classes: {
+      invalid : (formField) => formField.state().invalid() && formField.state().touched()
+    }
+  })]
 })
 export class BookCreatePage {
   protected readonly bookFormData = signal<Book>(
@@ -26,8 +40,8 @@ export class BookCreatePage {
   protected readonly bookForm = form(this.bookFormData, path => {
     {
       required(path.isbn, {message: 'ISBN is required.'});
-      minLength(path.isbn, 13 , {message: 'ISBN must be 13 characters.'});
-      maxLength(path.isbn, 13 , {message: 'ISBN must be 13 characters.'});
+      minLength(path.isbn, 13, {message: 'ISBN must be 13 characters.'});
+      maxLength(path.isbn, 13, {message: 'ISBN must be 13 characters.'});
       required(path.title, {message: "Title is required."});
       required(path.rating, {message: "Rating is required."});
       min(path.rating, 1, {message: "Rating must be at least 1."});
