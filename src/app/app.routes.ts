6742561ca628c578705513c0be5c860d5f43556f
u216116 <alina_maria.mylka@sbb.ch>
2026-06-22 11:10:22 +0200

    feat: added navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import {Routes} from '@angular/router';
 import {ErrorPage} from './error-page/error-page';
+import {HomePage} from './home-page/home-page';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'books', pathMatch: 'full'},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomePage, title: 'Home Page'},
   {
     path: 'books',
     loadChildren: () => import('./books/books.routes').then(m => m.bookRoutes)
--- src/app/books/books.routes.ts
+++ src/app/books/books.routes.ts
@@ -1,8 +1,12 @@
 import {Routes} from '@angular/router';
 import {BooksDetails} from './books-details/books-details';
 import {DashboardPage} from './dashboard-page/dashboard-page';
+import {BookCreatePage} from './book-create-page/book-create-page';
+import {BookSearchPage} from './book-search-page/book-search-page';
 
 export const bookRoutes: Routes = [
   {path: '', component: DashboardPage, title: 'Books'},
+  {path: 'create', component: BookCreatePage, title: 'Create Book'},
+  {path: 'search', component: BookSearchPage, title: 'Search Books'},
   {path: ':isbn', component: BooksDetails, title: 'Book Details'},
 ];
