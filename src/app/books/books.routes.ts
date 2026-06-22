0bfc477219f0cc4a3fc0ed0fc878ea879b26f6b2
u216116 <alina_maria.mylka@sbb.ch>
2026-06-22 11:50:38 +0200

    feat: added navigation

--- src/app/books/books.routes.ts
+++ src/app/books/books.routes.ts
@@ -3,10 +3,18 @@ import {BooksDetails} from './books-details/books-details';
 import {DashboardPage} from './dashboard-page/dashboard-page';
 import {BookCreatePage} from './book-create-page/book-create-page';
 import {BookSearchPage} from './book-search-page/book-search-page';
+import {BooksEntryPage} from './books-entry-page/books-entry-page';
 
 export const bookRoutes: Routes = [
-  {path: '', component: DashboardPage, title: 'Books'},
-  {path: 'create', component: BookCreatePage, title: 'Create Book'},
-  {path: 'search', component: BookSearchPage, title: 'Search Books'},
-  {path: ':isbn', component: BooksDetails, title: 'Book Details'},
+  {
+    path: '',
+    component: BooksEntryPage,
+    title: 'Books',
+    children: [
+      {path: '', component: DashboardPage, title: 'Dashboard'},
+      {path: 'create', component: BookCreatePage, title: 'Create Book'},
+      {path: 'search', component: BookSearchPage, title: 'Search Books'},
+      {path: ':isbn', component: BooksDetails, title: 'Book Details'}
+    ]
+  }
 ];
