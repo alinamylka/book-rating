b6bfda06dbd52bea63ca9f02d8839456cf163c97
u216116 <alina_maria.mylka@sbb.ch>
2026-06-24 10:38:09 +0200

    feat: submit button

--- src/app/books/dashboard-page/dashboard-page.ts
+++ src/app/books/dashboard-page/dashboard-page.ts
@@ -1,11 +1,11 @@
-import {ChangeDetectionStrategy, Component, DestroyRef, inject, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {Book} from '../shared/book';
 import {BookCard} from '../book-card/book-card';
 import {BookRatingHelper} from '../shared/book-rating-helper';
 import {BookStore} from '../shared/book-store';
 import {interval, map} from 'rxjs';
 import {DatePipe} from '@angular/common';
-import {takeUntilDestroyed, toSignal} from '@angular/core/rxjs-interop';
+import {toSignal} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -20,22 +20,22 @@ import {takeUntilDestroyed, toSignal} from '@angular/core/rxjs-interop';
 export class DashboardPage {
   readonly #bookStore: BookStore = inject(BookStore);
   readonly #helper: BookRatingHelper = inject(BookRatingHelper);
-  readonly #destroyRef = inject(DestroyRef);
-  readonly currentTime = signal<Date>(new Date());
-  readonly currentTimeWithSignal = toSignal(
+//  readonly #destroyRef = inject(DestroyRef);
+//  readonly currentTime = signal<Date>(new Date());
+  readonly currentTime = toSignal(
     interval(1000).pipe(
       map(() => new Date()),
-      takeUntilDestroyed(this.#destroyRef)
+   //   tap(e => console.log(e)),
     ),
     {initialValue: new Date()}
   );
-  readonly interval = setInterval(() => {
+/*  readonly interval = setInterval(() => {
     console.log(`tick: ${new Date()}`);
     this.currentTime.set(new Date())
-  }, 1000);
+  }, 1000);*/
 
   constructor() {
-    this.#destroyRef.onDestroy(() => clearInterval(this.interval));
+  //  this.#destroyRef.onDestroy(() => clearInterval(this.interval));
   }
 
 
